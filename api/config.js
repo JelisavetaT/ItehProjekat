a9f8edcbe839d86c11262283f6cc3fb823b2de30
JelisavetaT <jt20200140@student.fon.bg.ac.rs>
2024-07-01 14:31:39 +0200

    Api init

---
+++ api/config.js
@@ -0,0 +1,9 @@
+export default {
+  JWT_SECRET: 'pu4XN+MuBeIj6GfVdNi1/NzWeB/y0YJ32K+CZ9ArzDQ=',
+  ETHEREAL_EMAIL: 'susan.schroeder@ethereal.email',
+  ETHEREAL_PASS: '2rrbHWw3vMtJQMEDuP',
+  GMAIL_EMAIL: 'andrijanikoliciteh@gmail.com',
+  GMAIL_PASS: 'axbg hkcd wuoh wcgy',
+  MONGODB_CONN_STRING:
+    'mongodb+srv://andrija:iteh@cluster0.jqlq89k.mongodb.net/booking?retryWrites=true&w=majority',
+};
